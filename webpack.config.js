5de2ab18ee6465a4663b65b35039f3210cf0c3a6
tobq <tobi.akiny@gmail.com>
2019-04-23 20:07:22 +0100

    moved bundle location and ignored from git

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,6 @@ module.exports = {
     },
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'public')
+        path: path.resolve(__dirname, 'public/js')
     }
 };
